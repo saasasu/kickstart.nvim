0d10b63a89e4335d265b954536c31123a67bbce6
Sasu Saalasti <sasu.saalasti@esett.com>
2025-04-06 11:36:17 +0300

    feat: keymap for close buffer

--- init.lua
+++ init.lua
@@ -180,6 +180,8 @@ vim.keymap.set("n", "<leader>o", function()
   require("oil").open()
 end, { desc = "Open [O]il" })
 
+vim.keymap.set("n", "<leader>.", "<cmd>bd<CR>", { desc = "Close Buffer" })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
