c99e3c1dbf1181165b08eea753b3330f97a8077e
Sasu <55926848+saasasu@users.noreply.github.com>
2025-05-04 23:36:44 +0300

    feat: add nvim-lint command and put everthing in same file

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -1,12 +1,21 @@
 return {
 
   { -- Linting
-    'mfussenegger/nvim-lint',
-    event = { 'BufReadPre', 'BufNewFile' },
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
-      local lint = require 'lint'
+      local lint = require "lint"
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        markdown = { "markdownlint" },
+        lua = { "luacheck" },
+        python = { "flake8" },
+      }
+
+      lint.linters.luacheck.args = {
+        unpack(lint.linters.luacheck.args),
+        "--globals",
+        "love",
+        "vim",
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
@@ -43,18 +52,25 @@ return {
 
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
-      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-        group = lint_augroup,
-        callback = function()
-          -- Only run the linter in buffers that you can modify in order to
-          -- avoid superfluous noise, notably within the handy LSP pop-ups that
-          -- describe the hovered symbol using Markdown.
-          if vim.opt_local.modifiable:get() then
-            lint.try_lint()
-          end
-        end,
-      })
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+      vim.api.nvim_create_autocmd(
+        { "BufEnter", "BufWritePost", "InsertLeave" },
+        {
+          group = lint_augroup,
+          callback = function()
+            -- Only run the linter in buffers that you can modify in order to
+            -- avoid superfluous noise, notably within the handy LSP pop-ups that
+            -- describe the hovered symbol using Markdown.
+            if vim.opt_local.modifiable:get() then
+              lint.try_lint()
+            end
+          end,
+        }
+      )
+
+      vim.keymap.set("n", "<leader>l", function()
+        lint.try_lint()
+      end, { desc = "[L]int current file" })
     end,
   },
 }
