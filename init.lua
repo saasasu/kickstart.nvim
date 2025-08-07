6456ebbfbce0ea129fe96092be26f7898538f756
Sasu Saalasti <sasu.saalasti@uni.lux>
2025-08-07 12:57:06 +0200

    feat: add tab settings

--- init.lua
+++ init.lua
@@ -166,6 +166,12 @@ vim.o.scrolloff = 10
 -- See `:help 'confirm'`
 vim.o.confirm = true
 
+-- Tab settings
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
